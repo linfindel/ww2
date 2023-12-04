843810ec8e334182bcaf52027941da8e7ff387f4
Nadir Technologies <linfindel@gmail.com>
2023-12-04 21:18:08 +0000

    Merge with local project

--- filters.js
+++ filters.js
@@ -1,84 +1,76 @@
 function setFilter(filter, value) {
-    localStorage.setItem(filter, value);
+  localStorage.setItem(filter, value);
 
-    if (value == "on") {
-        document.getElementById(filter).onclick = () => {
-            setFilter(filter, "off");
-        };
+  if (value == "on") {
+    document.getElementById(filter).onclick = () => {
+      setFilter(filter, "off");
+    };
 
-        document.getElementById(filter).classList.add("card");
-        document.getElementById(filter).classList.add("interactive");
-        document.getElementById(filter).classList.remove("card-subtle");
-    }
-    
-    else {
-        document.getElementById(filter).onclick = () => {
-            setFilter(filter, "on");
-        };
+    document.getElementById(filter).classList.add("card");
+    document.getElementById(filter).classList.add("interactive");
+    document.getElementById(filter).classList.remove("card-subtle");
+  } else {
+    document.getElementById(filter).onclick = () => {
+      setFilter(filter, "on");
+    };
 
-        document.getElementById(filter).classList.remove("card");
-        document.getElementById(filter).classList.remove("interactive");
-        document.getElementById(filter).classList.add("card-subtle");
-    }
+    document.getElementById(filter).classList.remove("card");
+    document.getElementById(filter).classList.remove("interactive");
+    document.getElementById(filter).classList.add("card-subtle");
+  }
 
-    fetchSettings();
+  fetchSettings();
 }
 
 function getFilter(filter) {
-    return localStorage.getItem(filter);
+  return localStorage.getItem(filter);
 }
 
 function enableAll() {
-    fetch("items.json")
-    .then(respone => respone.json())
-    .then(data => {
-        data.forEach(item => {
-            setFilter(item, "on");
-        })
-    })
+  fetch("items.json")
+    .then((respone) => respone.json())
+    .then((data) => {
+      data.forEach((item) => {
+        setFilter(item, "on");
+      });
+    });
 }
 
 function fetchSettings() {
-    fetch('items.json')
-    .then(response => response.json())
-    .then(data => {
-        var on = 0;
-        var off = 0;
+  fetch("items.json")
+    .then((response) => response.json())
+    .then((data) => {
+      var on = 0;
+      var off = 0;
 
-        data.forEach(item => {
-            if (getFilter(item) == "off") {
-                off++;
+      data.forEach((item) => {
+        if (getFilter(item) == "off") {
+          off++;
 
-                document.getElementById(item).onclick = () => {
-                    setFilter(item, "on");
-                }
+          document.getElementById(item).onclick = () => {
+            setFilter(item, "on");
+          };
 
-                document.getElementById(item).classList.remove("card");
-                document.getElementById(item).classList.remove("interactive");
-                document.getElementById(item).classList.add("card-subtle");
-            }
-
-            else {
-                on++;
-            }
-        });
-
-        console.log("On:", on);
-        console.log("Off:", off);
-
-        if (off == 0) {
-            document.getElementById("enable-all").inert = true;
-            document.getElementById("enable-all").className = "button-subtle";
+          document.getElementById(item).classList.remove("card");
+          document.getElementById(item).classList.remove("interactive");
+          document.getElementById(item).classList.add("card-subtle");
+        } else {
+          on++;
         }
+      });
 
-        else {
-            document.getElementById("enable-all").inert = false;
-            document.getElementById("enable-all").className = "";
-        }
+      console.log("On:", on);
+      console.log("Off:", off);
+
+      if (off == 0) {
+        document.getElementById("enable-all").inert = true;
+        document.getElementById("enable-all").className = "button-subtle";
+      } else {
+        document.getElementById("enable-all").inert = false;
+        document.getElementById("enable-all").className = "";
+      }
     })
-    .catch(
-        error => console.error('Error:', error)
-    );
+    .catch((error) => console.error("Error:", error));
 }
 
 fetchSettings();
